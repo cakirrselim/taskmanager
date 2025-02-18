23b02b356ff2529d42ae431fbde3a980b39fb7e0
selimcakr <selim.cakir-35@hotmail.com>
2025-02-18 23:27:50 +0300

    Role

--- src/main/java/com/selim/taskmanager/rest/model/RoleAddRequestModel.java
+++ src/main/java/com/selim/taskmanager/rest/model/RoleAddRequestModel.java
@@ -2,5 +2,5 @@ package com.selim.taskmanager.rest.model;
 
 import java.util.UUID;
 
-public record RoleAddRequestModel(UUID id, String name, String description) {
+public record RoleAddRequestModel(String name, String description) {
 }
